33a071f951650c8c5c65acd950ea510b8957c866
evalex O <evalex7@gmail.com>
2025-11-10 19:28:59 +0000

    Error: [2025-11-10T19:26:16.223Z]  @firebase/firestore: "Firestore (11.9

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:347697555011:web:3ea4ac24fd47a1ecaf96f6",
   "apiKey": "AIzaSyAt206UqgFqb3kcrLg2QrlU0_UWTOkv5cM",
   "authDomain": "studio-4342411592-256bb.firebaseapp.com",
+  "storageBucket": "studio-4342411592-256bb.appspot.com",
   "measurementId": "",
   "messagingSenderId": "347697555011"
 };
