785235cb79be13e231c1676825ac25a935840f48
evalex O <evalex7@gmail.com>
2025-11-26 11:59:53 +0000

    Окей тепер зміни на versel відбулись все добре але є ще одне питання Ска

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,12 +1,10 @@
 'use client';
 
 import AppLayout from '@/components/AppLayout';
-import SettingsForm from '@/components/settings/SettingsForm';
 import CategoriesSettings from '@/components/settings/CategoriesSettings';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
-import ThemeSettings from '@/components/settings/ThemeSettings';
 import ChartSettings from '@/components/settings/ChartSettings';
 import CreditLimitSettings from '@/components/settings/CreditLimitSettings';
+import ThemeSettings from '@/components/settings/ThemeSettings';
 
 export default function SettingsPage() {
   return (
